d9f002718d0fc6da16e73896961e67087be32598
Muhammad Luthfi Khusyasy <khusyasy@gmail.com>
2021-08-18 09:46:21 +0700

    create user route get users -> array post login -> jwt token

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,19 @@
-exports.get_users = function(req, res, next) {
-  res.send('users datas');
+const mongoose = require('mongoose');
+const User = require('../models/User');
+const jwt = require("jsonwebtoken");
+
+exports.get_users = async function(req, res) {
+  let users = await User.find({}).limit(50);
+  res.status(200).json(users);
+}
+
+exports.login = async function(req, res) {
+  const user = await User.findOne({ email: req.body.email });
+  if (user === null) return res.status(401).json({ status: "failed", error: "Not Found" });
+  
+  const valid = await user.checkPassword(req.body.password);
+  if (!valid) return res.status(401).json({ status: "failed", error: "Wrong Password" });
+
+  const token = jwt.sign({ user: user._id }, process.env.JWT_SECRET);
+  res.status(200).json({ status: "success", token });
 }
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const usersController = require('../controllers/users');
+
 router.get('/', usersController.get_users);
+
+router.post('/login', usersController.login);
 
 module.exports = router;
