8a85e397377742c3867861ef487622b7c0a51734
Muhammad Luthfi Khusyasy <khusyasy@gmail.com>
2021-08-18 08:07:56 +0700

    db config

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
+require("./config/database");
+
 app.use('/users', usersRouter);
 
 module.exports = app;
