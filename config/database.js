f0f23e23efe0d25d3d63ffb2b42228e60ae7a43c
Muhammad Luthfi Khusyasy <khusyasy@gmail.com>
2021-08-18 08:02:25 +0700

    start project

---
+++ config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
